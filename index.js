e1ac01827eb36fef48e2251ad62f2a015adb3e25
Wasanta <kwan_u_day@hotmail.com>
2021-11-19 15:20:32 -0500

    commit tree 4568eb5f11567b610a6b1a81373d4f2724d2de70

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+function submitData(name,email){
+    fetch('http://localhost:3000/users',{
+        method: 'POST',
+        headers:{
+            "Content-Type": "application/json",
+            Accept:"application/json",
+        },
+        body: JSON.stringify({
+            name:name,
+            email:email
+        })
+    })
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function(object) {
+        let h2 = document.createElement('h2');
+        h2.innerHTML = object.id;
+        document.body.appendChild(h2);
+        console.log(object);
+    })
+    .catch(function(error) {
+        let h2 = document.createElement('h2');
+        h3.innerHTML = error.message;
+        document.body.appendChild(h2);
+        console.log(error.message);
+    });
+}
+submitData("craig","link");
